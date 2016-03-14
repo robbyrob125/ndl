e8db919a959c050294fd040467472d4ff86ed907
Camil Staps <info@camilstaps.nl>
2016-03-14 13:43:39 +0100

    Changes March 8th

--- erik/landmarks.py
+++ erik/landmarks.py
@@ -34,8 +34,9 @@ def main(ip):
     tts = getProxy(ip, 'TextToSpeech')
     motion = getProxy(ip, 'Motion')
     posture = getProxy(ip, 'RobotPosture')
+    tracker = getProxy(ip, 'Tracker')
 
-    subscribe(mark, 'LandMarks', 500, 0.0)
+    #subscribe(mark, 'LandMarks', 500, 0.0)
 
     # Stand
     StiffnessOn(motion)
@@ -43,29 +44,50 @@ def main(ip):
     posture.goToPosture("StandInit", 0.5)
 
     for part in ['Body', 'Head', 'Legs', 'Arms']:
-        motion.setBreathEnabled(part, False)
         motion.setIdlePostureEnabled(part, False)
-    motion.setAngles(['Head'], [0.0,0.0], 0.5)
+        motion.setBreathEnabled(part, False)
+    motion.setAngles(['Head'], [0.0,0.0], 0.8)
+    motion.setSmartStiffnessEnabled(True)
+
+    current = 107
+    tracker.registerTarget('LandMark', [0.4, [current]])
+    #tracker.setMaximumDistanceDetection(1000)
+    tracker.setMode('Move')
+    tracker.track('LandMark')
+    tts.say("%d" % current)
 
     while True:
-        detected = []
-        data = mem.getData('LandmarkDetected')
         try:
-            marks = data[1]
-            for [[_,alpha,beta,_,_,heading], [mark]] in marks:
-                detected.append((mark,alpha,beta,heading))
-            
-            if len(detected) > 1:
-                print("I'm totally confused")
-            elif len(detected) == 1:
-                mark, alpha, beta, heading = detected[0]
-                print("Following mark %d" % mark)
-                motion.moveTo(0, 0, alpha)
-                motion.moveTo(0.3, 0, 0)
-
-        except IndexError:
+            #pass
+            [x, y, z] = tracker.getTargetPosition(0)
+            print(x,y,z)
+            if abs(x) < 0.2 and abs(y) < 0.2:
+                current = 112 if current == 107 else 107
+                tts.say("I'm now following mark %d." % current)
+                tracker.registerTarget('LandMark', [0.4, [current]])
+                tracker.track('LandMark')
+        except ValueError, e:
             pass
-        time.sleep(0.5)
+
+        time.sleep(0.3)
+
+        #detected = []
+        #data = mem.getData('LandmarkDetected')
+        #try:
+        #    marks = data[1]
+        #    for [[_,alpha,beta,_,_,heading], [mark]] in marks:
+        #        detected.append((mark,alpha,beta,heading))
+        #    
+        #    if len(detected) > 1:
+        #        print("I'm totally confused")
+        #    elif len(detected) == 1:
+        #        mark, alpha, beta, heading = detected[0]
+        #        print("Following mark %d" % mark)
+        #        motion.moveTo(0, 0, alpha)
+        #        motion.moveTo(0.3, 0, 0)
+
+        #except (IndexError, TypeError):
+        #    pass
 
 if __name__ == '__main__':
     try:
@@ -77,6 +99,9 @@ if __name__ == '__main__':
 
     for proxy, event in subscriptions:
         proxy.unsubscribe(event)
+    
+    tracker = getProxy(NAO_IP, 'Tracker')
+    tracker.stopTracker()
+    tracker.unregisterAllTargets()
 
-    print('Goodbye')
-
+    print; print('Goodbye'); print
